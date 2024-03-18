73eced0e32663fd59ddaf8362968ea9269433e5d
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2024-03-18 20:27:25 +0900

    :bug: Fix searchBar consumerWidget -> hookWidget

--- lib/src/presentation/pages/home/home_page.dart
+++ lib/src/presentation/pages/home/home_page.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
+import 'package:github_search_app/src/core/router/routes.dart';
 import 'package:github_search_app/src/presentation/base/base_page.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 part 'widgets/search_bar.dart';
--- lib/src/presentation/pages/home/widgets/search_bar.dart
+++ lib/src/presentation/pages/home/widgets/search_bar.dart
@@ -1,11 +1,25 @@
 part of '../home_page.dart';
 
-class _SearchBar extends ConsumerWidget {
+class _SearchBar extends HookWidget {
   const _SearchBar();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final focusNode = useFocusNode();
+    useEffect(
+      () {
+        focusNode.addListener(
+          () {
+            if (focusNode.hasFocus) {
+              focusNode.unfocus();
+              context.pushNamed(SearchRouteData.name);
+            }
+          },
+        );
+        return focusNode.dispose;
+      },
+      const [],
+    );
     return Hero(
       tag: 'search',
       child: CupertinoSearchTextField(
