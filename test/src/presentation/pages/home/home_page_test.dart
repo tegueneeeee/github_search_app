f05678fce1e39dea77a7bb9d2db133a0fb98bfdf
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2024-04-02 07:44:26 +0900

    :white_check_mark: Refactor home page test and remove unused imports

--- test/src/presentation/pages/home/home_page_test.dart
+++ test/src/presentation/pages/home/home_page_test.dart
@@ -1,24 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../robot.dart';
-
 void main() {
   group(
     'HomePage',
-    () {
-      testWidgets(
-        'When tap search text field route to SearchPage',
-        (tester) async {
-          await tester.runAsync(
-            () async {
-              final robot = Robot(tester);
-              await robot.pumpAppWithFakes();
-              await robot.home.tapSearchButton();
-              robot.search.expectPage();
-            },
-          );
-        },
-      );
-    },
+    () {},
   );
 }
